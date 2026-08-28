5db7b827c6cfc9e81b7177fe03359d550f810fc8
Ted <teddyyongteng@gmail.com>
2026-08-28 19:14:02 +0800

    chore(v2): make carousel renderer self contained

--- v2/components/recent-memory-canvas.tsx
+++ v2/components/recent-memory-canvas.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import { useEffect, useMemo, useRef, useState } from "react";
 import type { LifeEvent, Media } from "@/lib/types";
 
